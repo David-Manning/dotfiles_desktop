4ea27f1abe019d984b86053b70eebb483d180c20
David-Manning <16976385+David-Manning@users.noreply.github.com>
2025-08-31 22:50:22 +0100

    Add nvim tabs configuration

---
+++ config/nvim/lua/config/ui/tabs.lua
@@ -0,0 +1,10 @@
+
+-- ~/.config/nvim/lua/config/ui/tabs.lua
+
+-- Tabs and indentation
+vim.opt.tabstop = 4        -- A tab character looks like 4 spaces
+vim.opt.shiftwidth = 4      -- Number of spaces for each indent level
+vim.opt.softtabstop = 4     -- Treat 4 spaces as a tab when editing
+vim.opt.expandtab = false   -- Use actual tab characters, not spaces
+vim.opt.autoindent = true   -- Copy indent from current line to new line
+
