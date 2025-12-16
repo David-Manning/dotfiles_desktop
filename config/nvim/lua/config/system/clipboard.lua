198f35c298097fabaf78b09ff3651b2c1b060932
David-Manning <16976385+David-Manning@users.noreply.github.com>
2025-12-16 16:42:09 +0000

    Add clipboard for Windows

--- config/nvim/lua/config/system/clipboard.lua
+++ config/nvim/lua/config/system/clipboard.lua
@@ -6,16 +6,17 @@
 
 vim.opt.clipboard = "unnamedplus"
 
--- Explicitly set clipboard provider for Wayland
-vim.g.clipboard = {
-  name = 'wl-clipboard',
-  copy = {
-    ['+'] = 'wl-copy',
-    ['*'] = 'wl-copy',
-  },
-  paste = {
-    ['+'] = 'wl-paste',
-    ['*'] = 'wl-paste',
-  },
-  cache_enabled = 0,
-}
+if vim.fn.has('win-32') == 1 then
+	vim.g.clipboard = 'unnamedplus'
+
+elseif vim.env.WAYLAND_DISPLAY then
+	-- Explicitly set clipboard provider for Wayland
+	vim.g.clipboard = {
+		name = 'wl-clipboard',
+		copy = {['+'] = 'wl-copy',
+				['*'] = 'wl-copy'},
+		paste = {['+'] = 'wl-paste',
+				 ['*'] = 'wl-paste'},
+		cache_enabled = 0}
+end
+
