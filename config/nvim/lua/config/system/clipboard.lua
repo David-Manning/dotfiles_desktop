aa681aa0b0923430866605297f75fa689d55c1fb
David-Manning <16976385+David-Manning@users.noreply.github.com>
2025-12-16 16:51:57 +0000

    Include explicit support for non-Windows and non-Wayland systems

--- config/nvim/lua/config/system/clipboard.lua
+++ config/nvim/lua/config/system/clipboard.lua
@@ -4,19 +4,22 @@
 -- CLIPBOARD (Wayland)
 -- ============================================================================
 
-vim.opt.clipboard = "unnamedplus"
 
 if vim.fn.has('win-32') == 1 then
 	vim.g.clipboard = 'unnamedplus'
 
 elseif vim.env.WAYLAND_DISPLAY then
 	-- Explicitly set clipboard provider for Wayland
+	vim.opt.clipboard = "unnamedplus"
 	vim.g.clipboard = {
+
 		name = 'wl-clipboard',
 		copy = {['+'] = 'wl-copy',
 				['*'] = 'wl-copy'},
 		paste = {['+'] = 'wl-paste',
 				 ['*'] = 'wl-paste'},
 		cache_enabled = 0}
+else
+	vim.opt.clipboard = "unnamedplus"
 end
 
